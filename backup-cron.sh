0dca868deb15235575de73f2d36d62db4754213f
Bar Galditzky <bar@farmdog.ag>
2019-12-02 13:40:47 +0200

    add slack alert

--- backup-cron.sh
+++ backup-cron.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+
+echo "Running backup script"
 ​
 HOST=elasticsearch1:9200
 SNAPSHOT_NAME=s3-snpahost
+SLACK_CHANNEL_WEBHOOK=webhook_url
+
 
 TIME=`/bin/date +%d-%m-%Y-%T`
 
 curl -XPUT "http://$HOST/_snapshot/$SNAPSHOT_NAME/$TIME" -d '{}'
+curl -XPUT $SLACK_CHANNEL_WEBHOOK -d '{"text": "backup made."}'
